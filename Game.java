e415b2d302f813ff0b30b5edfacaa011a5ec9ba8
Marcello Nascif <marcellobnf@aluno.puc-rio.br>
2023-04-16 15:35:53 -0300

    teste 2

--- Game.java
+++ Game.java
@@ -1,5 +1,14 @@
 public class Game{
+    int value;
+    String color;
+
     public construct(){
         int value = 0;
+    }
+
+    public construct(String color){
+        int value = 1;
+        color = this.color;
+
     }
 }

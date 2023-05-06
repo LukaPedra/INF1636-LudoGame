68b43f3bb629463c36aa1339747bc6fe0408b000
Marcello Nascif <marcellobnf@aluno.puc-rio.br>
2023-05-06 15:36:11 -0300

    Apagar RenderColor

--- Ludo/src/Model/Casa.java
+++ Ludo/src/Model/Casa.java
@@ -3,7 +3,6 @@ package Model;
 public class Casa {
 	TipoCasa tipo; // Casa comum = 0; Casa de saída = 1; Abrigo = 2
 	Cor corAssociada; //Se for uma casa especial uma cor pode ser associada a ela
-	RenderColor renderColor = RenderColor.branco; //Pinta a casa com essa variável (por padrão é branca)
 	
 	
 	public Casa(TipoCasa t) { // Construtor para Casa Comum e Abrigos
@@ -17,18 +16,12 @@ public class Casa {
 	public void setCorAssociada(Cor c){
 		corAssociada = c;
 	}
-	public void setRenderColor(RenderColor color){
-		renderColor = color;
-	}
-
+	
 	//Métodos para pegar as variaveis da classe
 	public TipoCasa getTipo(){
 		return tipo;
 	}
 	public Cor getCorAssociada(){
 		return corAssociada;
-	}
-	public RenderColor getRenderColor(){
-		return renderColor;
 	}
 }
--- Ludo/src/Model/RenderColor.java
+++
@@ -1,9 +0,0 @@
-package Model;
-
-public enum RenderColor {
-	//São 10 tipos diferentes de quadrados quando 
-	//for renderizar
-	branco, azul, amarelo, verde, 
-	vermelho, preto, saidaVerde, 
-	saidaVermelho, saidaAmarelo, saidaAzul;
-}

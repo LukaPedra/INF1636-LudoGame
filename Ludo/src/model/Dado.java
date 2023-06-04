5166987ecab718770d1c22c34069206c6cff4448
Marcello Nascif <marcellobnf@gmail.com>
2023-06-04 20:05:37 -0300

    Update Dado.java
    Tirei o jogador como parametro de rolar

--- Ludo/src/model/Dado.java
+++ Ludo/src/model/Dado.java
@@ -6,7 +6,7 @@ class Dado {
 	private Random random = new Random();
     private int resultado;
 	
-	public int rolar(Jogador j) {
+	public int rolar() {
 		resultado = random.nextInt(6) + 1;
 		return resultado;
 	}
